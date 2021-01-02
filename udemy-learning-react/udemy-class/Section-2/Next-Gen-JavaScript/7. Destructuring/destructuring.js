af233f27863d7eb1ee02f97bdaa883e57d7ac562
Austin Gray <Austin.m.gray210@gmail.com>
2021-01-01 23:45:19 -0600

    finished  Destructuring

---
+++ udemy-learning-react/udemy-class/Section-2/Next-Gen-JavaScript/7. Destructuring/destructuring.js
@@ -0,0 +1,23 @@
+"use strict";
+    
+/*
+Destructuring allows to easily extract single array elements or object
+properties and store the in variables
+ */
+
+// Array Destructuring
+// [a, b] = ['Hello', 'Austin']
+// console.log(a)  // Hello
+// console.log(b)  // Austin
+
+// const numbers = [1, 2, 3];
+// [num1, num2] = numbers;
+// console.log(num1, num2);
+
+
+// Object Destructuring is the same but with curly braces
+// {name} = {name: 'Austin', age: 28}
+// console.log(name)   // Austin
+// console.log(age)    // undefined
+
+
