d7b59e531c97fd51b7225003283567e42bf1c19c
Austin Gray <Austin.m.gray210@gmail.com>
2021-01-02 00:00:59 -0600

    finished RefreshingArrayFunctions

---
+++ udemy-learning-react/udemy-class/Section-2/Next-Gen-JavaScript/9. RefreshingArrayFunctions/refreshingArrayFunctions.js
@@ -0,0 +1,12 @@
+"use strict";
+    
+const numbers = [1, 2, 3];
+
+// turn this into an array where each number is doubled
+
+const doubleNumArray = numbers.map((num) => {
+    return num * 2;
+});
+
+console.log(numbers);
+console.log(doubleNumArray);
