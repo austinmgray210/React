ddd95423340996df04af916dcdef0125fb68d4ef
Austin Gray <Austin.m.gray210@gmail.com>
2021-01-01 22:57:44 -0600

    finished arrowFunctions

---
+++ udemy-learning-react/udemy-class/Section-2/Next-Gen-JavaScript/2. ArrowFunctions/arrowFunctions.js
@@ -0,0 +1,45 @@
+"use strict";
+    
+// Traditional arrow function syntax
+// function myFunction() {
+//     ...
+// }
+
+// Arrow function syntax
+// const myFunction = () => {
+//     ...
+// }
+
+// passing a variable
+// function printMyName(name) {
+//     console.log(name)
+// }
+// printMyName('Austin');
+
+// Arrow key with variable
+// const printMyName = (name) => {
+//     console.log(name);
+// }
+// printMyName('Austin');
+
+// You can omit the perenthesis if you're only passing 1 argument
+// const printMyName = name => {
+//     console.log(name);
+// }
+// printMyName('Austin');
+
+// Passing 2 variables
+// const printMyName = (name, age) => {
+//     console.log(name, age);
+// }
+// printMyName('Austin', 28);
+
+// multiplying numbers
+// const multiply = (number) => {
+//     return number * 2;
+// }
+// console.log(multiply(2));
+
+// simplified if only returning something
+// const multiply = number => number * 2;
+// console.log(multiply(2));
