1a580b01312cb8d1398325989bde71b127d65744
Austin Gray <Austin.m.gray210@gmail.com>
2021-01-01 22:47:06 -0600

    added section 2

---
+++ udemy-learning-react/udemy-class/Section-2/Next-Gen-JavaScript/1. LetAndConst/letAndConst.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// var is variable
+// var myName = 'Austin';
+// console.log(myName);
+// myName = 'Kathy';
+// console.log(myName);
+
+// let is still variable
+// let myName = 'Austin';
+// console.log(myName);
+// myName = 'Kathy';
+// console.log(myName);
+
+// const is not changable, redefining myName throws an error
+// const myName = 'Austin';
+// console.log(myName);
+//
+// myName = 'Kathy';
+// console.log(myName);
