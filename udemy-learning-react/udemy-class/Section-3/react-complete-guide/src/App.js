1fe5ecc6bcb19aae5d72a091cde7badabbf89153
Austin Gray <Austin.m.gray210@gmail.com>
2021-01-02 19:40:01 -0600

    learned useState() hook for State Manipulation

--- udemy-learning-react/udemy-class/Section-3/react-complete-guide/src/App.js
+++ udemy-learning-react/udemy-class/Section-3/react-complete-guide/src/App.js
@@ -1,20 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = props => {
+    const [ personsState, setPersonsState ] = useState({
+        persons: [
+            { name: 'Austin', age: 28 },
+            { name: 'Kathy', age: 28 },
+            { name: 'David', age: 50 }
+        ]
+    });
+
+    const [otherState, setOtherState] = useState('some other value');
+
+    console.log(personsState, otherState);
+
+    const switchNameHandler = () => {
+        // console.log('Was clicked!');
+        // DON'T DO THIS: this.state.persons[0].name = 'Austin Gray';
+        setPersonsState({
+            persons: [
+                { name: 'Austin Gray', age: 28 },
+                { name: 'Kathy', age: 28 },
+                { name: 'David', age: 54 }
+            ]
+        });
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-          <Person name="Austin" age="28" />
-          <Person name="Kathy" age="28" >My Hobbies: Racing</Person>
-          <Person name="David" age="50" />
+          <button onClick={switchNameHandler}>Switch Name</button>
+          <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+          <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies: Racing</Person>
+          <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-  }
 }
 
-export default App;
+export default app;
