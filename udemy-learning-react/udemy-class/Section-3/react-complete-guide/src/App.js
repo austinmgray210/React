3cebc56f58b617f3efc780868b6e7578b405fb96
Austin Gray <Austin.m.gray210@gmail.com>
2021-01-04 23:32:53 -0600

    added two way binding

--- udemy-learning-react/udemy-class/Section-3/react-complete-guide/src/App.js
+++ udemy-learning-react/udemy-class/Section-3/react-complete-guide/src/App.js
@@ -12,12 +12,22 @@ class App extends Component {
         otherState: 'Some other value'
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
         this.setState( {
             persons: [
-                { name: 'Austin Gray', age: 28 },
+                { name: newName, age: 28 },
                 { name: 'Kathy', age: 28 },
-                { name: 'David', age: 50 }
+                { name: 'David', age: 54 }
+            ]
+        })
+    }
+
+    nameChangedHandler = (event) => {
+        this.setState( {
+            persons: [
+                { name: 'Austin', age: 28},
+                { name: event.target.value, age: 28},
+                { name: 'David', age: 54}
             ]
         })
     }
@@ -27,14 +37,15 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working!</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <button onClick={() => this.switchNameHandler('Austin Gray!!')}>Switch Name</button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age} />
                 <Person
                     name={this.state.persons[1].name}
                     age={this.state.persons[1].age}
-                    click={this.switchNameHandler}>My Hobbies: Racing</Person>
+                    click={this.switchNameHandler.bind(this, 'Austin Gray!')}
+                    changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
                 <Person
                     name={this.state.persons[2].name}
                     age={this.state.persons[2].age} />
