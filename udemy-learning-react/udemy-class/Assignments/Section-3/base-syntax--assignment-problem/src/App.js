b3903d3e6f77c87aa2d7cbd81c3a5841ee7edab3
Austin Gray <Austin.m.gray210@gmail.com>
2021-01-08 18:54:30 -0600

    finished TODO

--- udemy-learning-react/udemy-class/Assignments/Section-3/base-syntax--assignment-problem/src/App.js
+++ udemy-learning-react/udemy-class/Assignments/Section-3/base-syntax--assignment-problem/src/App.js
@@ -32,10 +32,7 @@ class App extends Component {
 export default App;
 
 // TODO:
-//   Pass the event-handler method reference to the UserInput component and bind it to the input-change event
-//   Ensure that the new input entered by the user overwrites the old username passed to UserOutput
-//   Add two-way-binding to your input (in UserInput) to also display the starting username
-//   Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets
+
 
 // FINISHED:
 //   Create TWO new components: UserInput and UserOutput
@@ -44,6 +41,10 @@ export default App;
 //   Pass a username (of your choice) to UserOutput via props and display it there
 //   Add state to the App component (=> the username) and pass the username to the UserOutput component
 //   Add a method to manipulate the state (=> an event-handler method)
+//   Pass the event-handler method reference to the UserInput component and bind it to the input-change event
+//   Ensure that the new input entered by the user overwrites the old username passed to UserOutput
+//   Add two-way-binding to your input (in UserInput) to also display the starting username
+//   Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets
 
 
 
