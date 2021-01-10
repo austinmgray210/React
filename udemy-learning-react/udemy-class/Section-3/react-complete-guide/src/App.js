93b4d7f2a488d673d941e18c9440880eef25c939
austin <austin.m.gray210@gmail.com>
2021-01-09 22:25:36 -0600

    finished Flexible Lists

--- udemy-learning-react/udemy-class/Section-3/react-complete-guide/src/App.js
+++ udemy-learning-react/udemy-class/Section-3/react-complete-guide/src/App.js
@@ -20,14 +20,23 @@ class App extends Component {
       this.setState({persons: persons});
     };
 
-    nameChangedHandler = (event) => {
-        this.setState({
-            persons: [
-                {name: 'Austin', age: 28},
-                {name: event.target.value, age: 28},
-                {name: 'David', age: 54}
-            ]
-        })
+    nameChangedHandler = ( event, id ) => {
+        const personIndex = this.state.persons.findIndex(p => {
+            return p.id === id;
+        });
+
+        const person =  {
+            ...this.state.persons[personIndex]
+    };
+
+        // const person = Object.assign({}, this.state.persons[personIndex]);
+
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState({persons: persons} );
     };
 
     togglePersonsHandler = () => {
@@ -55,7 +64,8 @@ class App extends Component {
                             click={() => this.deletePersonHandler(index)}
                             name={person.name}
                             age={person.age}
-                            key={person.id} />
+                            key={person.id}
+                            changed={(event) => this.nameChangedHandler(event, person.id)} />
                     })}
                 </div>
             );
